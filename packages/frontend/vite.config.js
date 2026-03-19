df03e91bc671f0946dc5f1a4291255fc9069aeb7
unknown <eliefenohasina@gmail.com>
2026-03-19 17:18:47 +0300

    Env exemple

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['lavina-nongrieving-gabriella.ngrok-free.dev'],
     proxy: {
       '/api': {
         target:       'http://localhost:3000',

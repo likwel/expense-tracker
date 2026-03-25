07e08afe3cae4394fff818a4a3bcb55aa51cf36c
unknown <eliefenohasina@gmail.com>
2026-03-25 13:36:25 +0300

    Config vite.config

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
     allowedHosts: ['lavina-nongrieving-gabriella.ngrok-free.dev'],
     proxy: {
       '/api': {
-        target:       'http://localhost:3000',
+        target:       'http://localhost:9995',
         changeOrigin: true,
         secure:       false,
         // Optionnel : afficher les requêtes proxifiées dans le terminal
